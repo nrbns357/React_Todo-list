4ac0074c50a941f77048ad1450f85ceccda96c56
nrbns357 <gusgh357@gmail.com>
2021-09-05 16:43:15 +0900

    upDate

---
+++ nrbns-server/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/nav/index';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Nav/>
+    </>
+  );
+}
+
+export default App;
---
+++ velopert/src/App.js
@@ -0,0 +1,15 @@
+import React, { useRef } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function App() {
+  
+  return (
+    <>
+    
+    </>
+  );
+}
+
+
+export default App;
