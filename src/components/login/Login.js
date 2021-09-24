f9be1cdfed7f42c02c00348c47a0f11a968a1523
nrbns357 <gusgh357@gmail.com>
2021-09-25 02:41:31 +0900

    upDate

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,26 +1,15 @@
-import { useState } from "react";
-import Main from "../main/Main";
 import './Login.css';
 
 const Login = () => {
-    const [loginForm,setLoginForm]= useState(false);
-    const [user,setUser] = useState({
-        id: '',
-        pass:''
-    });
 
     return (
-        <>
-        <div className="main">
-                <input type="text" name="id" onChange={user.id}  placeholder="id" className="id input"></input>
-                <input type="password" name="pass" onChange={user.pass} placeholder="password" className="pass input"></input>
-                <input type="submit" onClick={()=>{
-                    fetch(`/API/login.php?id=${user.id?.target.value}&pass=${user.pass?.target.value}`)
-                    .then((response) => response.json());
-                }} className="sub"/>
-        </div>
-        {loginForm ? <Main/> : null}
-        </>
+        <main className="mainBar">
+            <div className="LoginFormContainer">
+               <input type="text" placeholder="아이디입력"/>
+               <input type="password" placeholder="비밀번호입력"/>
+               <input type="submit" value="로그인"/>
+            </div>
+        </main>
     );
 };
 
