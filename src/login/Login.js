31d6e2147e13f7ec2b4006b41db67affc3a1a1e4
nrbns357 <gusgh357@gmail.com>
2021-10-18 21:17:36 +0900

    upDate: Finding questions

--- src/login/Login.js
+++ src/login/Login.js
@@ -46,9 +46,13 @@ const Login = () => {
         e.preventDefault(); 
         const inputPass = Examine(); //true and false 값을 받아서 오는 함수
         if(inputPass){ //true면 try실행 , flase면 return해서
-        const res = await sendUserData(); // sendUserData();를 실행해서 return값을 res변수에 저장
+        const res = await sendUserData(); // {await}을 쓰는 이유는 비동기 처리 때문에 서버에서 값을 받기도 전에 다른 코드들을 실행 할 수 있어서 이것을 쓰면 동기 처리로 할 수 있기 떄문에 이 함수를 사용한다. 
+        //비동기 처리 (예를 들어 설명하면 서버에 요청을 하고 다른 코드를 실행을 할 수 있게 하는 복잡하지만 효율적인 처리이다. )
+        //동기 처리(그 반대인 동기는 예를 들어 서버에 요청을 하면 서버에 요청한 값을 가져오기 까지는 다른 코드를 실행 할 수 없는 쉽지만 비효율 적인 처리이다.)
+        // {sendUserData} 를 실행해서 return값을 res변수에 저장
         const {status, data} = res; // res변수에서 {status, data}만 뺀다. 
         dispatch(registTodo(data));
+        console.log(data);
           if(status === 200){ // {status}가 200일떄 (200은 성공했다는 의미이다.) 
             history.push("/Main"); // 참이면 페이지 이동
             return;  
--- src/main/Main.js
+++ src/main/Main.js
@@ -17,10 +17,23 @@ const TodoList = () => {
   };
 
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    dispatch(addTodo(value)); // 디스패치로 새로 입력한 text를 store에 보내준다.
+  const onSubmit = (event) => {  
+    // 문제점 1)  value를 입력했을 때 서버를 통해 데이터 베이스 (contnet)에 저장을 해야함. 
+    // 문제점 2)  value를 입력하고 제출을 하고 이전 페이지로 돌아 간 다음 어떤 아이디로든 로그인을 해도 입력했었던 contnent가 나옴
+    // 문제점 3)  user의 contnent가 안나옴
+    // 문제점 4)  로그인을 하고 이전 페이지로 돌아가고 다시 다른 아이디로 로그인을 하면 유저 정보가 안나옴 새로고침을 하고 다시 로그인을 해야 user의 정보가 나옴
+    // 문제점 5)  삭제 버튼을 누르면 서버에 ture값이 가긴 하지만 {console로도 나옴} 웹 페이지 상에는 안보임
+        event.preventDefault();
+    // if(value===null){ // value가 비었을 때 함수 실행을 안되게 해야함 (나중에 구현)
+    //   return;
+    // }else{
+
+    // }
+      dispatch(addTodo(value)); // 디스패치로 새로 입력한 text를 store에 보내준다.
+    console.log(value); 
     fetch(`/API/add.php?text=${value}`);
+    
+    
   };
 
   // const userDataBring = ()=>{
